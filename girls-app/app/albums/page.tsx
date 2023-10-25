fd65d861534d8a127eaf54806463185071f6fc0e
지수진 <zi_soo_zin@naver.com>
2023-10-25 19:45:36 +0900

    [추가] 앨범페이지 셋업

--- girls-app/app/albums/page.tsx
+++ girls-app/app/albums/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import Link from "next/link";
+// 더미데이터
+import { artists, records } from "../../data/hcode.js";
+/* 컴포넌트 */
+import HeaderComp from "../../app/components/HeaderComp";
+// 서브 CSS
+import "../../css/sub.css";
+
 export default function Album() {
   return (
-    <h2>앨범페이지</h2>
+    <>
+    <HeaderComp/>
+    <main className="contents_wrap">
+      <h2 className="mttit">DISCOGRAPHY</h2>
+      <div className="contents_inner">
+        <div className="album_list">
+          <ul>
+            {artists['alb'].map((x, i) => (
+              <li key={i}>
+                <Link href="/">
+                  <img src={x.isrc} alt={"여자아이들의 " + x.mtit + " 앨범"} />
+                  <div className="albumInfo_box">
+                    <div className="albumInfo_txt">
+                      <h5>{x.altop_txt.split("^")[1]}</h5>
+                      <h2>{x.mtit.replace(/-/g, "")}</h2>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </main>
+    </>
   )
 }
